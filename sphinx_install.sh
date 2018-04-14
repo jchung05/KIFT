d745c4db82db336edc74c524c20c26ce0f292f80
Ted <marixalijustice@gmail.com>
2018-04-13 22:09:42 -0700

    Adding brightness

--- sphinx_install.sh
+++ sphinx_install.sh
@@ -11,3 +11,6 @@ echo "export PATH=\"$HOME/.brew/bin:$PATH\"" >> ~/.zshrc
 brew tap watsonbox/cmu-sphinx
 brew install --HEAD watsonbox/cmu-sphinx/cmu-sphinxbase
 brew install --HEAD watsonbox/cmu-sphinx/cmu-pocketsphinx
+
+# Brightness
+brew install brightness
