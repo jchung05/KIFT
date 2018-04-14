3081adea9d239b2f9b08ee240edaec0a6c074e3c
Ted <marixalijustice@gmail.com>
2018-04-13 22:11:07 -0700

    Adding undone functions

---
+++ functions/brightness.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   brightness.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttran <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/13 21:25:29 by ttran             #+#    #+#             */
+/*   Updated: 2018/04/13 22:08:46 by ttran            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+/*
+** int system(const char *command) allows for the execution of shell commands
+** system("command");
+*/
+
+int brightness(char *brightness_value)
+{
+
+	return (0);
+}
+
+int main(void)
+{
+	brightness(5);
+	return (0);
+}
+
