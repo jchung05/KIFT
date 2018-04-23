ecf5364357d395fd9799daaefdf15094a58b6d8c
regien <gerardomalpartidavela@gmail.com>
2018-04-23 02:15:52 -0700

    messing around

--- pendejada.c
+++ pendejada.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   pendejada.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jchung <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: gmalpart <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/04/22 20:27:55 by jchung            #+#    #+#             */
-/*   Updated: 2018/04/23 02:14:44 by gmalpart         ###   ########.fr       */
+/*   Created: 2018/04/23 01:11:52 by gmalpart          #+#    #+#             */
+/*   Updated: 2018/04/23 02:14:43 by gmalpart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 #include <sphinxbase/err.h>
 #include <sys/time.h>
 #include <string.h>
-
 
 const char			*recognize_from_microphone(void);
 
@@ -33,32 +32,43 @@ char const			*decoded_speech;
 /**
 ** Remember to use the proper language model and dictionary
 ** when the corpus.txt is trained
+**
+** ps_args when no argument is passed it just NULL;
+**
+** -hmm = Directory containing acoustic model files
+** -lm = Word trigram language model input file
+** -dict = Main pronunciation dictionary (lexicon) input file
+** -logfn = Just redirecting logfiles
 */
 
-int					main(int argc, char **argv) {
-	config = cmd_ln_init(NULL, ps_args(), TRUE, 
-				"-hmm", MODELDIR "/en-us/en-us",
-				"-lm", MODELDIR "/en-us/en-us.lm.bin",
-				"-dict", MODELDIR "/en-us/cmudict-en-us.dict",
-				"-logfn", "/dev/null",
-				NULL);
+int			main(int ac, char **av)
+{
+	config = cmd_ln_init(NULL, ps_args(), TRUE,
+			"-hmm", MODELDIR "/en-us/en-us",
+			"-lm", MODELDIR "/en-us/en-us.lm.bin",
+			"-dict", MODELDIR "/en-us/cmudict-en-us.dict",
+			"-logfn", "/dev/null",
+			NULL);
+
 	if (!config)
 	{
-		fprintf(stderr, "Failed to create config object, see log for details\n");
+		fprintf(stderr, "Failed to create a config object, see log for details\n");
 		return (-1);
 	}
-
+	// initializing decoder that'simple
 	ps = ps_init(config);
 	if (!ps)
 	{
-		fprintf(stderr, "Failed to create recognizer, see log for details\n");
+		fprintf(stderr, "Failed to create a recognizer, see log for details\n");
 		return (-1);
 	}
 
-	printf("config done\n");
-	ad = ad_open_dev("sysdefault", (int) cmd_ln_float32_r(config, "-samprate"));
+	printf("Regular setup done\n");
+	// initialize a recording device  | int32 samples_per_sec - a.k.a sampling rate
+	// link = https://github.com/cmusphinx/sphinxbase/blob/master/src/libsphinxbase/fe/fe_interface.c
+	ad = ad_open_dev("sysdefault", (int)cmd_ln_float32_r(config, "-samprate"));
 
-	while(1)
+	while (1)
 	{
 		decoded_speech = recognize_from_microphone();
 		printf("You said: %s\n", decoded_speech);
